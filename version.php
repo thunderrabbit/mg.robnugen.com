3d9a41aba8cf2208dba61f53e5404bb6c482a970
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-02-13 09:10:01 +0900

    BEGIN allowing edit of multiple todos right on dashboard

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 
-const SENTIMENTAL_VERSION = "Show done dids instead of activities";
-const SEMVER = "0.9.0";
+const SENTIMENTAL_VERSION = "Allow easy editing of multiple todos";
+const SEMVER = "0.9.1";
