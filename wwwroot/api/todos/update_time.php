9aa2550da42b084662a6ad96e013ca6b8ea01741
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-02-24 16:13:18 +0900

    remove exception messages

--- wwwroot/api/todos/update_time.php
+++ wwwroot/api/todos/update_time.php
@@ -65,5 +65,5 @@ try {
 
 } catch (Exception $e) {
     http_response_code(500);
-    echo json_encode(['success' => false, 'error' => $e->getMessage()]);
+    echo json_encode(['success' => false, 'error' => 'An unexpected error occurred']);
 }
