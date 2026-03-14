bb69edc4786f77819a54e3fd4d55238b9e16da00
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-03-14 19:00:23 +0900

    BEGIN Inbox sort and filter controls

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN Hide past completed todos";
+const SENTIMENTAL_VERSION = "BEGIN Inbox sort and filter controls";
 const SEMVER = "0.10.3";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
