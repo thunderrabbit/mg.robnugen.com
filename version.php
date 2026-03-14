faa6f127955ef11c62873cd8349dd68eb071a44a
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-03-14 12:08:44 +0900

    BEGIN Hide past completed todos

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN Delete todo function";
-const SEMVER = "0.10.2";
+const SENTIMENTAL_VERSION = "BEGIN Hide past completed todos";
+const SEMVER = "0.10.3";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
