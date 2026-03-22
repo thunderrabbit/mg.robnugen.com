0214c9c3a70fcb3e744e005026c73154a5bf4d03
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-03-22 18:31:40 +1030

    BEGIN creating Agent_Inbox Users

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN UX Fixes";
-const SEMVER = "0.10.4";
+const SENTIMENTAL_VERSION = "BEGIN creating Agent_Inbox Users";
+const SEMVER = "0.11.0";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
