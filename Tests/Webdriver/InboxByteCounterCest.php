b45e477962c8a9a81361828aa4ac87b1b245f1c2
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2026-03-25 23:55:17 +0000

    Add Webdriver tests for inbox byte counter UI

---
+++ Tests/Webdriver/InboxByteCounterCest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Webdriver;
+
+use Tests\Support\AcceptanceTester;
+
+class InboxByteCounterCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->loginAsPaid();
+    }
+
+    public function mainTextareaCountsAsciiBytes(AcceptanceTester $I)
+    {
+        $I->amOnPage('/inbox/');
+        $I->waitForElementVisible('#message', 10);
+
+        // Type 5 ASCII characters = 5 bytes
+        $I->fillField('#message', 'Hello');
+        $I->waitForText('5 / 10,240 bytes', 5, '#message-counter');
+    }
+
+    public function mainTextareaCountsBytesNotCharacters(AcceptanceTester $I)
+    {
+        $I->amOnPage('/inbox/');
+        $I->waitForElementVisible('#message', 10);
+
+        // Emoji 😋 is 4 bytes in UTF-8, but 1 character
+        $I->fillField('#message', '😋');
+        $I->waitForText('4 / 10,240 bytes', 5, '#message-counter');
+
+        // Japanese あ is 3 bytes in UTF-8, but 1 character
+        $I->fillField('#message', 'あ');
+        $I->waitForText('3 / 10,240 bytes', 5, '#message-counter');
+
+        // Mix: "aあ😋" = 1 + 3 + 4 = 8 bytes
+        $I->fillField('#message', 'aあ😋');
+        $I->waitForText('8 / 10,240 bytes', 5, '#message-counter');
+    }
+
+    public function counterShowsWarningNearLimit(AcceptanceTester $I)
+    {
+        $I->amOnPage('/inbox/');
+        $I->waitForElementVisible('#message', 10);
+
+        // 9,217 bytes = just over 90% of 10,240 (threshold is 9,216)
+        // Use JS to set the value since fillField with 9K+ chars is slow
+        $I->executeJS("
+            var ta = document.getElementById('message');
+            ta.value = 'x'.repeat(9217);
+            ta.dispatchEvent(new Event('input'));
+        ");
+        $I->waitForElementVisible('.byte-counter-warn', 5);
+        $I->seeElement('#message-counter.byte-counter-warn');
+
+        // Over 100%: should have byte-counter-over class
+        $I->executeJS("
+            var ta = document.getElementById('message');
+            ta.value = 'x'.repeat(10241);
+            ta.dispatchEvent(new Event('input'));
+        ");
+        $I->waitForElementVisible('.byte-counter-over', 5);
+        $I->seeElement('#message-counter.byte-counter-over');
+    }
+
+    public function counterResetsWhenCleared(AcceptanceTester $I)
+    {
+        $I->amOnPage('/inbox/');
+        $I->waitForElementVisible('#message', 10);
+
+        $I->fillField('#message', 'Hello');
+        $I->waitForText('5 / 10,240 bytes', 5, '#message-counter');
+
+        // Clear the textarea
+        $I->fillField('#message', '');
+        $I->waitForText('0 / 10,240 bytes', 5, '#message-counter');
+
+        // Warning class should be gone
+        $I->dontSeeElement('#message-counter.byte-counter-warn');
+        $I->dontSeeElement('#message-counter.byte-counter-over');
+    }
+}
