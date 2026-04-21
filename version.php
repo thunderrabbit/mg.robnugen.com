0968a710d5df0774c96eecb56dbd39c1c225a6cf
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-04-22 05:44:53 +0930

    BEGIN #14 show done issues toggle

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN #12 repository management UI";
-const SEMVER = "0.12.0";
+const SENTIMENTAL_VERSION = "BEGIN #14 show done issues toggle";
+const SEMVER = "0.14.0";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
