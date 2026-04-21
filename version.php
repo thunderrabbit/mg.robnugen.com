d38196bebbf139c3fefed1c8a6e7f5879a97af00
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-04-22 07:19:58 +0930

    BEGIN #50 clean up issue create/edit page

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN #14 show done issues toggle";
-const SEMVER = "0.14.0";
+const SENTIMENTAL_VERSION = "BEGIN #50 clean up issue create/edit page";
+const SEMVER = "0.14.1";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
