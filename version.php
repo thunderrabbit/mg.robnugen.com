d1cffecd540e70fad06f957777b650ada84cc8f9
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-04-21 17:25:58 +0930

    BEGIN adding priority field to issues

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN adding can_read_project / can_write_project permission bits";
-const SEMVER = "0.11.2";
+const SENTIMENTAL_VERSION = "BEGIN adding priority field to issues";
+const SEMVER = "0.11.3";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
