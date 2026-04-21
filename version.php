c8c35f2a1a17530071e7100daf3cb7a82b357931
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-04-21 17:49:18 +0930

    BEGIN write-side UI forms for project/issue management

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN adding priority field to issues";
-const SEMVER = "0.11.3";
+const SENTIMENTAL_VERSION = "BEGIN write-side UI forms for project/issue management";
+const SEMVER = "0.11.4";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
