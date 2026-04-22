942a5c32ec7bc95df6a64d009e5202a7bbd78580
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-04-22 12:21:20 +0930

    BEGIN #3 flaky-test handling strategy

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN #62 PATCH /issues/update ambiguous description column";
-const SEMVER = "0.14.3";
+const SENTIMENTAL_VERSION = "BEGIN #3 flaky-test handling strategy";
+const SEMVER = "0.14.4";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
