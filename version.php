34d046e4996c76b24f8a23630c1777d47604ee47
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-04-23 18:52:56 +0930

    BEGIN #74 Fix format of new issue page

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN #75 Move back button to left";
-const SEMVER = "0.14.7";
+const SENTIMENTAL_VERSION = "BEGIN #74 Fix format of new issue page";
+const SEMVER = "0.14.8";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
