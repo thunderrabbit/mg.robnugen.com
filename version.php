156c3fb43acf3dcb823a6847f84dd1c8d7a0c49d
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-04-23 15:44:22 +0930

    BEGIN #77 Included assignee dropdown when create new issues

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN #48 CreateProjectCest DB pollution fix";
-const SEMVER = "0.14.5";
+const SENTIMENTAL_VERSION = "BEGIN #77 Included assignee dropdown when create new issues";
+const SEMVER = "0.14.6";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
