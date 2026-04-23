0679e107a8a2f90630ffa471d88189757d57397f
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-04-23 18:58:52 +0930

    BEGIN #76 Show status, created date, and sort issues

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "BEGIN #74 Fix format of new issue page";
-const SEMVER = "0.14.8";
+const SENTIMENTAL_VERSION = "BEGIN #76 Show status, created date, and sort issues";
+const SEMVER = "0.14.9";
 
 const EMOTIONAL_SESSION_GAP_MINUTES = 30;
